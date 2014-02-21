121d7b3302e489ac95d73c51df94acc6d1e5e08c
Shaenei <James@Shaenei.com>
2014-02-21 19:15:41 +0000

    Initial Commit

---
+++ xcode/BeerSong/BeerSong/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  BeerSong
+//
+//  Created by James Jackson on 21/02/2014.
+//  Copyright (c) 2014 James Jackson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
